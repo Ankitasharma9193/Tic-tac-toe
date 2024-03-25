c72c28e6cb6e9ded6c1d42033e6cd6db0a60fcb4
Ankita Sharma <ankitasharma9193@gmail.com>
2024-03-25 03:16:44 -0400

    implement line, winning pattern x,y for line

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ const changeTurn = (turn) => {
 const checkWin = () => {
   let boxtext = document.getElementsByClassName('boxtext');
   let wins = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0,1,2, 5, 5, 0],
+    [3,4,5, 5, 15, 0],
+    [6,7,8, 5, 25, 0],
+    [0,3,6, -5, 15, 90],
+    [1,4,7, 5, 15, 90],
+    [2,5,8, 15, 15, 90],
+    [0,4,8, 5, 15, 45],
+    [2,4,6, 5, 15, 135]
   ]
 
   wins.forEach((win) => {
@@ -30,6 +30,11 @@ const checkWin = () => {
         && boxtext[win[1]].innerText === boxtext[win[2]].innerText){
             isGameOver = true;
             document.querySelector('.turnInfo').innerText = `Player ${boxtext[win[0]].innerText} won!`;
+
+            // line will be visible after the win, we do not want to show it initially thus we set width to 0
+            document.querySelector('.line').style.width = '25vw'
+            document.querySelector('.line').style.transform = `translate(${win[3]}vw, ${win[4]}vw) rotate(${win[5]}deg)`
+
             console.log(document.querySelector('.imageClass'));
             document.querySelector('.imageClass').getElementsByTagName('img')[0].style.width = '200px';
         }
@@ -58,6 +63,7 @@ Array.from(boxes).forEach((eachbox) => {
 // for reset button
 let resetButton = document.getElementsByClassName("resetButton")[0];
 console.log(resetButton);
+
 resetButton.addEventListener('click', () => {
     let boxtext = document.getElementsByClassName( "boxtext" );
     Array.from(boxtext).forEach((item) => {
@@ -65,6 +71,7 @@ resetButton.addEventListener('click', () => {
     } )
     isGameOver = false; // since we reset
     turn = 'X';
+    document.querySelector('.line').style.width = '0vw'; // line needs to be disappear 
     document.getElementsByClassName('turnInfo')[0].innerText = 'Your Turn: ' + turn; // the text should be  X's turn again for as was initially
     document.querySelector('.imageClass').getElementsByTagName('img')[0].style.width = '0px'; // the width of the gif should be 0 again as was initially
 })
